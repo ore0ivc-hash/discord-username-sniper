6824974fac1e598555245964ea91c4ca6e12f554
ore0ivc-hash <ore0ivc@gmail.com>
2026-07-25 01:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { checkUsername } from './utils/check.js';
 import { claimUsername } from './utils/claim.js';
-import turbo from 'faster-axios';
+import axios from 'axios';
 function readLines(filePath) {
   try {
     const data = fs.readFileSync(filePath, 'utf8').trim();
